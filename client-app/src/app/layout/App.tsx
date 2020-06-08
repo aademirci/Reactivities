f20c7584021530f34310f96cc38b454bb896d2e3
A.A. Demirci <aalperdemirci@gmail.com>
2020-06-08 17:09:13 +0300

    End of sectıon 13

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useContext, useEffect } from 'react';
 import { Container } from 'semantic-ui-react'
 import NavBar from '../../features/nav/NavBar';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
@@ -9,11 +9,31 @@ import HomePage from '../../features/home/HomePage';
 import ActivityDetails from '../../features/activities/details/ActivityDetails';
 import NotFound from './NotFound';
 import { ToastContainer } from 'react-toastify'
+import LoginForm from '../../features/user/LoginForm';
+import { RootStoreContext } from '../stores/rootStore';
+import LoadingComponent from './LoadingComponent';
+import ModalContainer from '../common/modals/ModalContainer';
 
 const App: React.FC<RouteComponentProps> = ({ location }) => {
+  const rootStore = useContext(RootStoreContext)
+  const {setAppLoaded, token, appLoaded} = rootStore.commonStore
+  const {getUser} = rootStore.userStore
+
+  useEffect(() => {
+    if (token) {
+      getUser().finally(() => setAppLoaded())
+    } else {
+      setAppLoaded()
+    }
+  }, [getUser, setAppLoaded, token])
+
+  if (!appLoaded) {
+    return <LoadingComponent content='Loading app' />
+  }
 
   return (
-    <div>
+    <Fragment>
+      <ModalContainer />
       <Route exact path='/' component={HomePage} />
       <Route path={'/(.+)'} render={() => (
         <Fragment>
@@ -24,12 +44,13 @@ const App: React.FC<RouteComponentProps> = ({ location }) => {
               <Route exact path='/activities' component={ActivityDashboard} />
               <Route path='/activities/:id' component={ActivityDetails} />
               <Route key={location.key} path={['/createActivity', '/manage/:id']} component={ActivityForm} />
+              <Route path='/login' component={LoginForm} />
               <Route component={NotFound} />
             </Switch>
           </Container>
         </Fragment>
       )} />
-    </div>
+    </Fragment>
   );
 
 }
--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -1,19 +1,20 @@
 import React, { useContext, Fragment } from 'react'
 import { Item, Segment, Label } from 'semantic-ui-react'
 import { observer } from 'mobx-react-lite'
-import ActivityStore from '../../../app/stores/activityStore'
 import ActivityListItem from './ActivityListItem'
+import { RootStoreContext } from '../../../app/stores/rootStore'
+import { format, parseISO } from 'date-fns/esm'
 
 const ActivityList: React.FC = () => {
-    const activityStore = useContext(ActivityStore)
-    const {activitiesByDate} = activityStore
+    const rootStore = useContext(RootStoreContext)
+    const {activitiesByDate} = rootStore.activityStore
 
     return (
         <Fragment>
             {activitiesByDate.map(([group, activities]) => (
                 <Fragment key={group}>
                     <Label key={group} size='large' color='blue'>
-                        {group}
+                        {format(parseISO(group), 'eeee do MMMM, yyyy')}
                     </Label>
                     <Segment clearing>
                         <Item.Group divided>
