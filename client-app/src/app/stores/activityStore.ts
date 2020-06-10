a75a17a0c040b2b5a1ef695e85d6a3e541c9eaaf
A.A. Demirci <aalperdemirci@gmail.com>
2020-06-10 23:49:54 +0300

    End of section 21

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -1,4 +1,4 @@
-import { observable, action, computed, runInAction } from 'mobx'
+import { observable, action, computed, runInAction, reaction } from 'mobx'
 import { SyntheticEvent } from 'react'
 import { IActivity } from '../models/activity'
 import agent from '../api/agent'
@@ -8,11 +8,21 @@ import { RootStore } from './rootStore'
 import { setActivityProps, createAttendee } from '../common/util/util'
 import { HubConnection, HubConnectionBuilder, LogLevel } from '@microsoft/signalr'
 
+const LIMIT = 2
 
 export default class ActivityStore {
     rootStore: RootStore
     constructor(rootStore: RootStore) {
         this.rootStore = rootStore
+
+        reaction(
+            () => this.predicate.keys(),
+            () => {
+                this.page = 0
+                this.activityRegistry.clear()
+                this.loadActivities()
+            }
+        )
     }
 
     @observable activityRegistry = new Map()
@@ -22,9 +32,30 @@ export default class ActivityStore {
     @observable target = ''
     @observable loading = false
     @observable.ref hubConnection: HubConnection | null = null
+    @observable activityCount = 0
+    @observable page = 0
+    @observable predicate = new Map()
 
     @computed get activitiesByDate() {
         return this.groupActivitiesByDate(Array.from(this.activityRegistry.values()))
+    }
+
+    @computed get totalPages() {
+        return Math.ceil(this.activityCount / LIMIT)
+    }
+
+    @computed get axiosParams() {
+        const params = new URLSearchParams()
+        params.append('limit', String(LIMIT))
+        params.append('offset', `${this.page ? this.page * LIMIT : 0}`)
+        this.predicate.forEach((value, key) => {
+            if (key === 'startDate') {
+                params.append(key, value.toISOString())
+            } else {
+                params.append(key, value)
+            }
+        })
+        return params
     }
 
     groupActivitiesByDate(activities: IActivity[]) {
@@ -40,12 +71,14 @@ export default class ActivityStore {
         this.loadingInitial = true
 
         try {
-            const activities =  await agent.Activities.list()
+            const activitiesEnvelope =  await agent.Activities.list(this.axiosParams)
+            const { activities, activityCount } = activitiesEnvelope
             runInAction('loading activities', () => {
                 activities.forEach((activity) => {
                     setActivityProps(activity, this.rootStore.userStore.user!)
                     this.activityRegistry.set(activity.id, activity)
                 })
+                this.activityCount = activityCount
                 this.loadingInitial = false
             }) 
         } catch (error) {
@@ -230,4 +263,14 @@ export default class ActivityStore {
         }
     }
 
+    @action setPage = (page: number) => {
+        this.page = page
+    }
+
+    @action setPredicate = (predicate: string, value: string | Date) => {
+        this.predicate.clear()
+        if (predicate !== 'all') {
+            this.predicate.set(predicate, value)
+        }
+    }
 }
